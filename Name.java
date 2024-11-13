f4391cdbf5d12b0e9038f57dd0364329b9c1c6f2
Sneha <snehajosephmuringathery@gmail.com>
2024-11-14 02:10:33 +0530

    adding java file

---
+++ Name.java
@@ -0,0 +1,13 @@
+class Name
+{
+
+      public static void main(String arg[])
+	  {
+	             System.out.println("Sneha Anil");
+				 System.out.println("Twenty eight");
+				 System.out.println("Edappally");
+				 
+				 }
+				 
+				 
+				 }
